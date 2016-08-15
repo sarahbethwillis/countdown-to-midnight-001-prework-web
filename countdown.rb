2b691f141ffe63156e0a53e9b5b8de539f73fa6c
Sarah Willis <sarahbethwillis@gmail.com>
2016-08-15 14:41:53 +0000

    Countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
-
-def countdown
+def countdown(n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+  break if n == 0
+  n -= 1
 end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep(1)
+  break if n == 0
+  n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+  end
